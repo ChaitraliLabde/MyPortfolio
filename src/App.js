2229a5fdf392524096a3bebf30a5830df87fb536
ChaitraliLabde <labdechaitrali@gmail.com>
2024-07-15 19:13:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-// App.js
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './Header';
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Skills from './Skills';
 import ContactMe from './ContactMe';
 import Footer from './Footer';
+import AdminDashboard from './AdminDashboard';
+import Login from './Login';
+import Blogs from './Blogs';
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">About Me</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/skills">Skills</Link>
-            </li>
-            <li>
-              <Link to="/contactme">Contact Me</Link>
-            </li>
-          </ul>
-        </nav>
+      <Header />
+      <main>
         <Routes>
           <Route path="/" element={<AboutMe />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/skills" element={<Skills />} />
-          <Route path="/contactme" element={<ContactMe />} />
+          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="*" element={<Navigate to="/admin-dashboard" />} />
         </Routes>
-        <Footer />
-      </div>
+      </main>
+      <Footer />
     </Router>
   );
 }
 
 export default App;
+/**/
